d7f81ac2f9e1419eda2f259c45acd56615b9bc26
JohnTESlade <john@jtes.net>
2012-05-13 11:19:51 +0100

    Move attr creation to initialiser

--- azulejo/unittests.py
+++ azulejo/unittests.py
@@ -71,17 +71,41 @@ class AzulejoTest(unittest.TestCase):
             [0, 0, 700, 1000]
         )
 
+    @patch.object(keybinder, 'bind')
+    def test_maximise(self, mock_my_method):
+        """ Test the maximising of active window """  
+        
+        keybinding_obj = KeyBinderDummy()
 
+        # Defined sideeffect for the key bind operation
+        def side_effect(key, dispatcher, dispatcher_params):
+            keybinding_obj.bind(key, dispatcher, dispatcher_params)
+        mock_my_method.side_effect = side_effect
+
+        # Monkey patch
+        azulejo_screen.AzulejoScreen = SingleTestScreenMock
+
+        # Import and run the code under test
+        import azulejo
+        azulejo.run(True)
+
+        # Trigger a keypress
+        keybinding_obj.action_key('<Super>1')
+
+        self.assertEqual(
+            keybinding_obj.get_screen().get_active_window()['geometry'],
+            [0, 0, 1000, 1000]
+        )
+        
 
 class KeyBinderDummy:
     """ Class this is used to allow keybindings to be caught and to be actioned """
 
-    bindings = []
-    saved_obj = None
-
     def __init__(self):
+    
+        self.bindings = []
+        self.saved_obj = None
 
-        pass
 
     def bind(self, action, dispatcher, dispatcher_params):
         """ Bind a key press """
