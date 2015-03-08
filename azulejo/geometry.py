679a232592fece964b2e506f6a1e1783e1ea78da
JohnTESlade <john@jtes.net>
2015-03-08 17:56:03 +0000

    Refactor is_similar

--- azulejo/geometry.py
+++ azulejo/geometry.py
@@ -4,31 +4,18 @@ class Geometry(object):
     """ A class to hold the geometry of a window or screen """
 
     def __init__(self, x=None, y=None, width=None, height=None):
-
         self.x = x
         self.y = y
         self.width = width
         self.height = height
 
-    def _as_list(self):
-        """ Returns the class a list [x, y, width, height] """
-
-        return [self.x, self.y, self.width, self.height]
-
-
     def is_similar(self, rhs, error_margin=30):
         """ Compare if objects are similar within the given error margin """
 
-        # TODO there might be a neater way to do this
-
-        ga = self._as_list()
-        gb = rhs._as_list()
-
-        for i in range(4):
-            if abs(ga[i] - gb[i]) >= error_margin:
+        for i in ('x', 'y', 'width', 'height'):
+            if abs(getattr(self, i) - getattr(rhs, i)) >= error_margin:
                 return False
         return True
-
 
     def __eq__(self, rhs):
         """ Comparison of object using vals """
